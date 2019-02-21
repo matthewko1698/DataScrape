33a5dbabf430bce2d882701777497bfe234395c3
Matthew Ko <matthew.ko@centre.edu>
2019-02-21 15:46:14 -0500

    1.2

--- trans2.js
+++ trans2.js
@@ -2,5 +2,5 @@ var s=document.createElement("script");
 s.src="https://d3js.org/d3.v5.js";
 var body = document.getElementsByTagName("body")[0];
 body.after(s);
-var studies = d3.selectAll(".fusion-text a").nodes();
-studies.forEach(function(a){console.log(a.innerText)})
+var chemaj = d3.selectAll("#pattern a").nodes();
+chemaj.forEach(function(a){console.log(a.innerText)});
